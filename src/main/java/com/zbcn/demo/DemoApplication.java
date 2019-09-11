119dc1af26887dbe8cbb3d2cd8f71117b41da83c
zbcn <1363653611@qq.com>
2019-09-11 17:18:20 +0800

    修改报错

--- src/main/java/com/zbcn/demo/DemoApplication.java
+++ src/main/java/com/zbcn/demo/DemoApplication.java
@@ -1,22 +1,13 @@
 package com.zbcn.demo;
 
-import com.zbcn.demo.thread.concurrency.HttpFilter;
-import com.zbcn.demo.thread.concurrency.HttpInterceptor;
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.ObjectProvider;
+import com.zbcn.demo.http.HttpFilter;
+import com.zbcn.demo.http.HttpInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import static org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration.*;
 
 @SpringBootApplication
 public class DemoApplication extends WebMvcConfigurerAdapter {
---
+++ src/main/java/com/zbcn/demo/http/RequestHolder.java
@@ -0,0 +1,23 @@
+package com.zbcn.demo.http;
+
+/**
+ * @Description:
+ * @Auther: zbcn
+ * @Date: 2/27/19 17:54
+ */
+public class RequestHolder {
+
+    public final static ThreadLocal<Long> requestHolder = new ThreadLocal<>();
+
+    public static void add(Long value){
+        requestHolder.set(value);
+    }
+
+    public static Long get(){
+        return requestHolder.get();
+    }
+
+    public static void remove(){
+        requestHolder.remove();
+    }
+}
